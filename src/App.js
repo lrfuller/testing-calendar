11b38aa2db4eb6fbb35874b0025004f46f47073a
LukasFuller <33261077+lrfuller@users.noreply.github.com>
2023-08-28 09:09:22 -0500

    initialize calendar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function App() {
+  const [dateVal, setDateVal] = useState(new Date());
+  const [parseDate, setParseDate] = useState(dateVal);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  useEffect(() => {
+    setParseDate(
+      dateVal.getDay() + " " + dateVal.getDate() + " " + dateVal.getFullYear()
+    );
+  }, [dateVal]);
+
+  const showDate = () => {
+    // setShowDatePicker(() => (prevState) => !prevState);
+    setShowDatePicker(!showDatePicker);
+  };
+
+  const dateChange = (e) => {
+    setDateVal(e);
+    setShowDatePicker(!showDatePicker);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ position: "absolute", top: "50px" }}>
+          testing react calendar
+          <div>
+            <input type="text" value={parseDate} disabled />
+            <input type="button" onClick={showDate} value="date" />
+          </div>
+        </div>
+        {showDatePicker === false ? (
+          <Calendar onChange={dateChange} value={dateVal} />
+        ) : null}
       </header>
     </div>
   );
